5b3fa204da50ede49cd993d07bacb22c8ae15124
Sarthak <sarthak.hti@gmail.com>
2026-05-28 12:48:11 +0530

    Initial HRIMS setup

---
+++ src/main/java/com/hti/hrims/HrimsApplication.java
@@ -0,0 +1,14 @@
+package com.hti.hrims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrimsApplication.class, args);
+		System.out.print("hello world");
+	}
+
+}
